370c9fc2722a0fa20d6e12aa0418273b4dcf24a2
Diogenes Fernandes <diofeher@gmail.com>
2025-05-20 12:03:09 -0300

    changing amd64 to x86_64
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- build/downloader.ts
+++ build/downloader.ts
@@ -36,16 +36,17 @@ function getPlatform(platform: string) {
 function getArch(arch: string) {
   // platform | terraform-ls  | extension platform | vs code editor
   //    --    |           --  |         --         | --
-  // macOS    | darwin_amd64  | darwin_x64         | ✅
+  // macOS    | darwin_x86_64  | darwin_x64         | ✅
   // macOS    | darwin_arm64  | darwin_arm64       | ✅
-  // Linux    | linux_amd64   | linux_x64          | ✅
+  // Linux    | linux_x86_64   | linux_x64          | ✅
   // Linux    | linux_arm     | linux_armhf        | ✅
   // Linux    | linux_arm64   | linux_arm64        | ✅
-  // Windows  | windows_amd64 | win32_x64          | ✅
+  // Windows  | windows_x86_64 | win32_x64          | ✅
   // Windows  | windows_arm64 | win32_arm64        | ✅
   if (arch === 'x64') {
-    return 'amd64';
+    return 'x86_64';
   }
+
   if (arch === 'armhf') {
     return 'arm';
   }
